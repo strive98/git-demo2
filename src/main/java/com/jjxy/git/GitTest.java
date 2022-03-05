78dd5565a00eeef85f1b628d64b64b9494dac9e0
QXR <1692050925@qq.com>
2022-03-05 12:00:27 +0800

    second commit

--- src/main/java/com/jjxy/git/GitTest.java
+++ src/main/java/com/jjxy/git/GitTest.java
@@ -3,5 +3,6 @@ package com.jjxy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
