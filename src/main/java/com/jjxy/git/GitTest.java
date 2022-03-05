f98b8ac647fcbe26d68d5fe8db93f83d133d829f
QXR <1692050925@qq.com>
2022-03-05 12:34:47 +0800

    push commit2

--- src/main/java/com/jjxy/git/GitTest.java
+++ src/main/java/com/jjxy/git/GitTest.java
@@ -6,6 +6,8 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("push test");
+        System.out.println("push test");
+        System.out.println("push test");
 
     }
 }
